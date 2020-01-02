2fd31a1823cbd351ac2ec75e39000d44f499987e
Dean Verleger <deanverleger@gmail.com>
2020-01-02 17:27:41 -0600

    work on gui

--- Start.ps1
+++ Start.ps1
@@ -54,14 +54,18 @@ function Show-Header {
   Write-Host "  / (_ / _ ``/  ' \/ -_)   / / / /  ' \/ -_)  "
   Write-Host "  \___/\_,_/_/_/_/\__/   /_/ /_/_/_/_/\__/   "
   Write-Host "                                             "
-  Write-Host "  Bal: $(Get-Balance)"
+  Write-Host "  Bal: $(Get-Balance)         "
 
 }
 
 
 function Show-Footer {
   Write-Host "                                             "
-  Write-Host "                         Press (Q) to quit   "
+  if ($global:showEsc) {
+    Write-Host "  <- (Esc)               Press (Q) to quit   "
+  } else {
+    Write-Host "                         Press (Q) to quit   "
+  }
   Write-Host "                                             "
 }
 
@@ -105,6 +109,7 @@ function Get-PaddedString {
 function Init {
   $global:quit = $false
   $global:section = ''
+  $global:subPage = ''
   $global:menuPositionX = 0
   $global:menuPositionY = 0
   $global:maxMenuPositionsX = 0
@@ -112,31 +117,95 @@ function Init {
   $global:containerWidth = 39
   $global:canChangeMenuPositonX = $false
   $global:canChangeMenuPositonY = $false
+  $global:showEsc = $false
 }
 
 function Init-MainMenu {
   $global:section = $sectionMainMenu
+  $global:subPage = ''
   $global:menuPositionX = 0
   $global:menuPositionY = 0
   $global:maxMenuPositionsX = 0
   $global:maxMenuPositionsY = 3
   $global:canChangeMenuPositonY = $true
   $global:canChangeMenuPositonX = $false
+  $global:showEsc = $false
 }
 
 function Init-JobsMenu {
   $global:section = $sectionJobsMenu
+  $global:subPage = ''
   $global:menuPositionX = 0
   $global:menuPositionY = 0
   $global:maxMenuPositionsX = 3
   $global:maxMenuPositionsY = 0
   $global:canChangeMenuPositonX = $true
   $global:canChangeMenuPositonY = $false
+  $global:showEsc = $true
+  Init-JobsSubSection
+}
+
+function Init-JobsSubSection {
+  $global:canChangeMenuPositonX = $true
+  $global:canChangeMenuPositonY = $false
+
+  # get sub section jobs
+  $posX = $global:menuPositionX
+  switch ($posX) {
+    0 {
+      $jobType = 'Quest'
+    } 1 {
+      $jobType = 'Daily'
+    } 2 {
+      $jobType = 'Rare'
+    }
+  }
+  $jobs = (Get-Jobs) | Where-Object { $_.Type -eq $jobType }
+  $global:currentJobs = $jobs
+  $jobs = , $jobs
+
+  # allow vertical nav if multiple jobs
+  $global:canChangeMenuPositonY = $jobs.Length -gt 1
+}
+
+function Init-JobsSingle {
+  $global:subPage = $jobPageSingle
+  $global:menuPositionY = 0
+  $global:maxMenuPositionsY = 3
+  $global:canChangeMenuPositonX = $false
+  $global:canChangeMenuPositonY = $true
+}
+
+function Init-GameMenu {
+  $global:section = $sectionGameMenu
+  $global:subPage = ''
+  $global:menuPositionX = 0
+  $global:menuPositionY = 0
+  $global:maxMenuPositionsX = 0
+  $global:maxMenuPositionsY = 0
+  $global:canChangeMenuPositonX = $false
+  $global:canChangeMenuPositonY = $false
+  $global:showEsc = $true
+}
+
+function Init-LogsMenu {
+  $global:section = $sectionLogsMenu
+  $global:subPage = ''
+  $global:menuPositionX = 0
+  $global:menuPositionY = 0
+  $global:maxMenuPositionsX = 0
+  $global:maxMenuPositionsY = 0
+  $global:canChangeMenuPositonX = $false
+  $global:canChangeMenuPositonY = $false
+  $global:showEsc = $true
 }
 
 # constants
 $sectionMainMenu = 'Main Menu'
 $sectionJobsMenu = 'Jobs'
+$sectionGameMenu = 'Game Time'
+$sectionLogsMenu = 'Logs'
+$jobPageSingle = 'single'
 
 function Show-MainMenu {
   $width = $global:containerWidth
@@ -165,10 +234,11 @@ function Show-JobsMenu {
   $isQuest = $posX -eq 0
   $isDaily = $posX -eq 1
   $isRare = $posx -eq 2
+  $jobs = $global:currentJobs
 
   Write-Host ""
   if ($isQuest) {
-    Write-Host "   .-------, ________  _______"
+    Write-Host "   .-------, ________  _______               "
     Write-Host "  / Quest /_/ Daily /_/ Rare /____________   "
   }
   elseif ($isDaily) {
@@ -176,26 +246,111 @@ function Show-JobsMenu {
     Write-Host "  / Quest /_/ Daily /_/ Rare /____________   "
   }
   elseif ($isRare) {
-    Write-Host "   ________  ________  .------,"
+    Write-Host "   ________  ________  .------,              "
     Write-Host "  / Quest /_/ Daily /_/ Rare /____________   "
   }
   Write-Host "  |$(Get-PaddedString)|  "
+  Show-JobCheckBoxes $jobs
+  Write-Host "  |$(Get-PaddedString)|  "
+  Write-Host "  |$(Get-PaddedString '  Press [Enter] to select')|  "
   Write-Host "  |$(Get-PaddedString -Fill '_')|  "
   Write-Host ""
-  # Write-Host "  |  (E)dit job                           |  "
   # Write-Host "  |  (R)emove job                         |  "
   # # Write-Host "  (B)alance"
   # Write-Host "  |  (S)pend Game Time points             |  "
   # Write-Host "  |  (V)iew logs                          |  "
 }
 
+function Show-JobsSingle {
+  $pos = $global:menuPositionY
+  $job = Get-CurrentJob
+  $jobTitle = $job.Title
+  $jobRate = $job.Rate
+  $jobRateSuffix = ""
+  if ($job.Type -eq "Quest") {
+    $jobRateSuffix = " per hour"
+  }
+  Write-Host ""
+  Write-Host "   $(Get-PaddedString -Fill '_')  "
+  Write-Host "  |$(Get-PaddedString)|  "
+  Write-Host "  |$(Get-PaddedString "  $jobTitle")|  "
+  Write-Host "  |$(Get-PaddedString "  (rewards: $jobRate$jobRateSuffix)")|  "
+  Write-Host "  |$(Get-PaddedString)|  "
+  $completeLine = Get-PaddedString "  $(Get-CheckBox ($pos -eq 0))(C)omplete"
+  Write-Host "  |$completeLine|  "
+  $editLine = Get-PaddedString "  $(Get-CheckBox ($pos -eq 1))(E)dit"
+  Write-Host "  |$editLine|  "
+  $removeLine = Get-PaddedString "  $(Get-CheckBox ($pos -eq 2))(R)emove"
+  Write-Host "  |$removeLine|  "
+  Write-Host "  |$(Get-PaddedString -Fill '_')|  "
+  Write-Host ""
+}
+
+function Show-GameMenu {
+  $width = $global:containerWidth
+  Write-Host ""
+  Write-Host "   .-----------,"
+  # Write-Host "  / Game Time /___________________________   "
+  Write-Host "  $(Get-PaddedString '/ Game Time /' '_' ($width+1))  "
+  Write-Host "  |$(Get-PaddedString)|  "
+  Write-Host "  |$(Get-PaddedString -Fill '_')|  "
+  Write-Host ""
+}
+
+function Show-LogsMenu {
+  $width = $global:containerWidth
+  Write-Host ""
+  Write-Host "   .------,"
+  # Write-Host "  / Logs /___________________________   "
+  Write-Host "  $(Get-PaddedString '/ Logs /' '_' ($width+1))  "
+  Write-Host "  |$(Get-PaddedString)|  "
+  Write-Host "  |$(Get-PaddedString -Fill '_')|  "
+  Write-Host ""
+}
+
+function Show-JobCheckBoxes {
+  param(
+    [Parameter(Mandatory = $true, Position = 0)]
+    $jobs
+  )
+  $jobs = , $jobs
+  $pos = $global:menuPositionY
+  for ($i = 0; $i -lt $jobs.Length; $i++) {
+    $job = $jobs[$i]
+    $selected = $pos -eq $i
+    $jobLine = "  $(Get-CheckBox $selected) $($job.Title)"
+    Write-Host "  |$(Get-PaddedString $jobLine)|  "
+  }
+}
+
+function Get-CurrentJob {
+  $pos = $global:menuPositionY
+  $jobs = $global:currentJobs
+  if ($jobs -and $jobs.Length -ge ($pos - 1) ) {
+    return $jobs[$pos]
+  }
+  return $false
+}
+
 function Show-BodyContent {
-  switch ($global:section) {
-    $sectionMainMenu {
-      Show-MainMenu
-    } $sectionJobsMenu {
+  $section = $global:section
+  if ($section -eq $sectionMainMenu) {
+    Show-MainMenu
+  }
+  elseif ($section -eq $sectionJobsMenu) {
+    $jobPage = $global:subPage
+    if (!$jobPage) {
       Show-JobsMenu
     }
+    elseif ($jobPage -eq $jobPageSingle) {
+      Show-JobsSingle
+    }
+  }
+  elseif ($section -eq $sectionGameMenu) {
+    Show-GameMenu
+  }
+  elseif ($section -eq $sectionLogsMenu) {
+    Show-LogsMenu
   }
 }
 
@@ -233,14 +388,12 @@ function Read-PositionInput {
   if (!$foundMatch) {
     if ($global:canChangeMenuPositonY) {
       if ($character -eq [System.ConsoleKey]::DownArrow) {
-        'down'
         if ($global:menuPositionY -lt ($global:maxMenuPositionsY - 1)) {
           $global:menuPositionY++
           $foundMatch = $true
         }
       }
       elseif ($character -eq [System.ConsoleKey]::UpArrow) {
-        'up'
         if ($global:menuPositionY -gt 0) {
           $global:menuPositionY--
           $foundMatch = $true
@@ -254,6 +407,8 @@ function Read-PositionInput {
 function Read-Input {
   $character = Read-Character
   $foundMatch = Read-PositionInput $character
+  $section = $global:section
+  $subPage = $global:subPage
 
   # section specific input
   if (!$foundMatch) {
@@ -265,9 +420,9 @@ function Read-Input {
           0 {
             Init-JobsMenu
           } 1 {
-
+            Init-GameMenu
           } 2 {
-
+            Init-LogsMenu
           }
         }
       }
@@ -285,11 +440,47 @@ function Read-Input {
     }
     #  jobs menu
     elseif ($section -eq $sectionJobsMenu) {
+      if (!$subPage) {
+        if ($character -eq [System.ConsoleKey]::Escape) {
+          Init-MainMenu
+        }
+        elseif ($character -eq [System.ConsoleKey]::Enter) {
+          $global:prevMenuPositionX = $global:menuPositionX
+          Init-JobsSingle
+        }
+      }
+      elseif ($subPage -eq $jobPageSingle) {
+        if ($character -eq [System.ConsoleKey]::Escape) {
+          Init-JobsMenu
+          $global:menuPositionX = $global:prevMenuPositionX
+          $global:prevMenuPositionX = 0
+        }
+        # elseif ($character -eq [System.ConsoleKey]::Enter) {
+
+        # }
+      }
+    }
+    # game menu
+    elseif ($section -eq $sectionGameMenu) {
       if ($character -eq [System.ConsoleKey]::Escape) {
         Init-MainMenu
       }
     }
-
+    # logs menu
+    elseif ($section -eq $sectionLogsMenu) {
+      if ($character -eq [System.ConsoleKey]::Escape) {
+        Init-MainMenu
+      }
+    }
+  } else {
+    # direction instruction
+    if ($section -eq $sectionJobsMenu) {
+      if (!$subPage) {
+        if ($character -eq [System.ConsoleKey]::LeftArrow -or $character -eq [System.ConsoleKey]::RightArrow) {
+          Init-JobsSubSection
+        }
+      }
+    }
   }
   Read-QuitInput $character
 }
