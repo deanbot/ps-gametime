ef356de339d6fba383161e8c82f5cbe7da8aa253
Dean Verleger <deanverleger@gmail.com>
2020-10-29 08:18:34 -0500

    pass params to start script #30

--- Source/Start.ps1
+++ Source/Start.ps1
@@ -1,16 +1,17 @@
 # ascii logo via http://patorjk.com/software/taag/
 param(
-  # optional suffix to db file names
-  [Parameter(Mandatory = $false, Position = 0)]
-  [string]$SessionName = "",
-
   # alternate storage directory
+  [Parameter(Mandatory = $false, Position = 0)]
+  [string]$StorageLocation = "",
+
+  # optional suffix to db file names
   [Parameter(Mandatory = $false, Position = 1)]
-  [string]$StorageLocation = ""
+  [string]$SessionName = ""
 )
 
 $Global:SessionName = $SessionName
 $Global:StorageLocation = $StorageLocation
+
 $Global:ScriptRoot = $PSScriptRoot
 
 . $Global:ScriptRoot\Main.ps1
--- bin/gametime.bat
+++ bin/gametime.bat
@@ -1,2 +1,2 @@
 @echo off
-powershell "%~dp0\..\Source\Start.ps1"
+powershell "%~dp0\..\Source\Start.ps1" %arg1% %arg2%
