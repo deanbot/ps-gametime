a2a37e953dd420423ec08a2480538dd8ea572626
kristapants <peachykeen@gmail.com>
2020-10-31 13:44:03 -0500

    Jobs index: added text for no quests present. Removed select a quest text when no quests to select. Logs index: added text for no logs present

--- Source/Views/Jobs/Index.psm1
+++ Source/Views/Jobs/Index.psm1
@@ -9,15 +9,28 @@ function Show-JobsMenu {
   Write-Host ""
   Show-JobsTabs $posX
   Write-Host "  |$(Get-PaddedString)|  "
-  Write-Host "  |$(Get-PaddedString "  Select a $($Label_Single_Quest_Lower): " -Width $widthLeft)$(Get-PaddedString "[N] New  " -Right $true -Width $widthRight)|  "
+  if ($jobs) {
+    Write-Host "  |$(Get-PaddedString "  Select a $($Label_Single_Quest_Lower): " -Width $widthLeft)$(Get-PaddedString "[N] New  " -Right $true -Width $widthRight)|  "
+  } 
+  else {
+    Write-Host "  |$(Get-PaddedString " " -Width $widthLeft)$(Get-PaddedString "[N] New  " -Right $true -Width $widthRight)|  "
+  }
   Write-Host "  |$(Get-PaddedString)|  "
 
+  $jobType = Get-CurrentJobType
   if ($jobs) {
     Show-JobCheckBoxes $jobs
   }
   else {
-    $jobType = Get-CurrentJobType
-    Write-Host "  |$(Get-PaddedString "  No $jobType $Label_Plural_Quest found.")|  "
+    if ($jobType -eq "Quest") {
+      Write-Host "  |$(Get-PaddedString "  No $Label_Plural_Quest found." -Center $true)|  "
+      
+    }
+    else {
+      Write-Host "  |$(Get-PaddedString "  No $jobType $Label_Plural_Quest found." -Center $true)|  "
+    }
+    Write-Host "  |$(Get-PaddedString)|  "
+    Write-Host "  |$(Get-PaddedString "Create a $Label_Single_Quest_Lower to see it here." -Center $true)|  "
   }
   # Write-Host "  |$(Get-PaddedString "  ")|  "
   # $addLine = "    [N] New job"
