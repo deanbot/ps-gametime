7237a9c174fc4e760505b956c6cd1770d79b076e
Dean Verleger <deanverleger@gmail.com>
2020-10-31 08:42:08 -0500

    add reset points #41

--- Source/Controllers/OptionsController.psm1
+++ Source/Controllers/OptionsController.psm1
@@ -1,5 +1,28 @@
+if ($Global:Debug) {
+  $DebugPreference = $Global:Debug
+}
+else {
+  $DebugPreference = "SilentlyContinue"
+}
+
 function Reset-Points {
   # add deduct transaction to set balance to 0
+  $balance = [decimal](Get-Balance)
+  $date = Get-Date -format 'MM/dd/yyyy'
+  $transaction = [PSCustomObject]@{
+    Date   = $date
+    JobId  = -1
+    Change = ($balance * -1)
+    Log    = "Reset Points"
+    Note   = ""
+  }
+  if ($success) {
+    Write-Debug "Transaction created successfully."
+  }
+  else {
+    Write-Debug "Transaction not created."
+  }
+  $success = Add-TransactionDb $transaction
 }
 
 # remove jobs and logs files
